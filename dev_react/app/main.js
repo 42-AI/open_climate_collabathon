5a0014f898199e3018b8e5c26e3dda0120e0e8bf
ArthurL <arthur.lanter@gmail.com>
2019-11-25 14:50:58 +0530

    Clean previous repo TABLE & EDIT gitignore

--- dev_react/app/main.js
+++ dev_react/app/main.js
@@ -6,18 +6,6 @@ import { createStore } from 'redux'
 import rootReducer from './reducers'
 import App from './containers/App';
 import statesData from './data/states-data';
-// import fetch_map_data from './api/fetch_map_data'
-
-// async function fetch_map_data () {
-// 	try {
-// 	  const map_data = await fetch('/data/API_data_output.json')
-// 	  console.log(`Here are data : ${map_data}`)
-// 	  return map_data;
-// 	} catch (error) {
-// 	  console.log(`${error} - Couldn't fetch data`)
-// 	}
-//   }
-// fetch_map_data()
 
 fetch('https://my-json-server.typicode.com/ArthurLan/fake_api/us_data')
 .then(resp => {
