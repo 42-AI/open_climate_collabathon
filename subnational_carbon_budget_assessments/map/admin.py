757dab5ed32f98fd2767159264b038ec5224481b
eparisot <eparisot@student.42.fr>
2019-11-27 17:15:34 +0100

    rename dev_django

--- subnational_carbon_budget_assessments/map/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import DataFile, Maps, States, Series, Points
-
-admin.site.register(DataFile)
-admin.site.register(Maps)
-admin.site.register(States)
-admin.site.register(Series)
-admin.site.register(Points)
